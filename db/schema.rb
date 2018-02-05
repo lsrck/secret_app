540fdb2855d2cd7edd9be72dea52a1535f04a107
Louis Aurick <louisaurick@ntymail.com>
2018-02-05 14:05:02 +0100

    crud

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180205110128) do
+ActiveRecord::Schema.define(version: 20180205113154) do
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
